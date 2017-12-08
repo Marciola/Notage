d415e2dbe40f35089f23923bf3f78cf69777173c
brandonp29 <brandonpolonia@gmail.com>
2017-12-08 12:43:20 -0500

    Add files via upload
    updated with gui

--- notepage.py
+++ notepage.py
@@ -9,12 +9,12 @@ def encodeMessage(img, message):
 
     newImage = Image.new(originalImage.mode, originalImage.size)
     newImage.putdata(pixels)
-    newImage.save(img, originalImage.format)
+    newImage.save("example.png" ,originalImage.format)
 
 def decodeMessage(img):
     count = 0
     message = ""
-    
+
     originalImage = Image.open(img)
     pixels = list(originalImage.getdata())
 
@@ -22,8 +22,8 @@ def decodeMessage(img):
     and pixels[count + 3] != 1 and pixels[count + 4] != 7:
         message += decodeChar(pixels[count])
         count += 1
-        
-    return message
+
+    print(message)
 
 def insertMessage(pixels, message):
     messageLength = len(message)
@@ -49,18 +49,22 @@ def encodeChar(char):
 def decodeChar(num):
     return chr(num)
 
+
 # Only one arg, decode message
-if len(sys.argv) == 2:
-    print sys.argv[1] + " has the message: " + decodeMessage(sys.argv[1])		
+#if len(sys.argv) == 2:
+ #   print sys.argv[1] + " has the message: " + decodeMessage(sys.argv[1])
 
 # Two args, encode message
-elif len(sys.argv) == 3:
-    print "Encoding..."
-    encodeMessage(sys.argv[1], sys.argv[2])
-    print sys.argv[1] + " has been encoded with the message '" + sys.argv[2] + "'"
+#elif len(sys.argv) == 3:
+ #   print "Encoding..."
+  #  encodeMessage(sys.argv[1], sys.argv[2])
+   # print sys.argv[1] + " has been encoded with the message '" + sys.argv[2] + "'"
+
+
 
 # Error handling
-else:
+"""else:
     print "Usage: image:png message:string"
     print "Decoding... notage.py image"
     print "Encoding... notage.py image message"
+"""
